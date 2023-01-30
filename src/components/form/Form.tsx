db75492cc541561ddd31f3e1ab28e7453fe5efbb
Lucas Bravi <lbravi@mobilecomputing.com.ar>
2023-01-30 18:18:42 -0300

    Se agrega select a formulario - sin terminar

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,6 +2,7 @@ import './index.scss'
 
 import { useState } from 'react'
 
+import { FormInterface } from '../../interfaces/formInterface'
 import ErrorMessage from '../error-message/ErrorMessage'
 import FormInput from '../form-input/FormInput'
 import FormLabel from '../form-label/FormLabel'
@@ -10,7 +11,7 @@ import SuccessMessage from '../success-message/SuccessMessage'
 
 const Form = ({ submitText, inputs, onSubmit, successMessage, errorMessage }:
     {
-        submitText: string, inputs: { inputName: string, inputText: string, inputType: string }[], onSubmit: Function, successMessage: string,
+        submitText: string, inputs: FormInterface[], onSubmit: Function, successMessage: string,
         errorMessage: string
     }) => {
 
@@ -21,21 +22,41 @@ const Form = ({ submitText, inputs, onSubmit, successMessage, errorMessage }:
     return (
         <form className="form" onSubmit={() => setFormData({})}>
             {
-                inputs && inputs.map((input, index) => (
+                inputs && inputs.map((input, index) =>
                     <div className="form__fields" key={index}>
-                        <FormLabel
-                            inputName={input.inputName}
-                            inputText={input.inputText}
-                        />
-                        <FormInput
-                            inputType={input.inputType}
-                            inputText={input.inputText}
-                            inputName={input.inputName}
-                            formData={formData}
-                            setFormData={setFormData}
-                        />
+                        {
+                            input.inputType === 'select' ? (
+                                <>
+                                    <FormLabel
+                                        inputName={input.inputName}
+                                        inputText={input.inputText}
+                                    />
+                                    <select name={input.inputName} title={input.inputText} className="form__select">
+                                        {
+                                            input.options && input.options.map((option, ind) => (
+                                                <option key={ind} value={option.id}>{option.nombre}</option>
+                                            ))
+                                        }
+                                    </select>
+                                </>
+                            ) : (
+                                <>
+                                    <FormLabel
+                                        inputName={input.inputName}
+                                        inputText={input.inputText}
+                                    />
+                                    <FormInput
+                                        inputType={input.inputType}
+                                        inputText={input.inputText}
+                                        inputName={input.inputName}
+                                        formData={formData}
+                                        setFormData={setFormData}
+                                    />
+                                </>
+                            )
+                        }
                     </div>
-                ))
+                )
             }
 
             <SubmitButton
--- src/root/App.tsx
+++ src/root/App.tsx
@@ -11,6 +11,8 @@ import NavigationButton from '../components/navigation-button/NavigationButton'
 import IngredientPage from '../containers/create-ingredient/IngredientPage'
 import RecipePage from '../containers/create-recipe/RecipePage'
 import HomePage from '../containers/home/HomePage'
+import RecipeIngredientPage
+  from '../containers/recipe-ingredient/RecipeIngredientPage'
 import ShowRecipe from '../containers/show-recipe/ShowRecipe'
 
 const App = () => {
@@ -34,14 +36,20 @@ const App = () => {
                             routeName="Ingredientes"
                             className="navigation-bar__link"
                         />
+                        <NavigationButton
+                            link="/recipe-ingredients"
+                            routeName="Agregar ingredientes"
+                            className="navigation-bar__link"
+                        />
                     </nav>
                 </aside>
                 <main>
                     <Switch>
                         <Route exact path="/" component={HomePage} />
                         <Route path="/recipe-show/:recipeId" component={ShowRecipe} />
-                        <Route path="/recipes" component={RecipePage} />
+                        <Route exact path="/recipes" component={RecipePage} />
                         <Route exact path="/ingredients" component={IngredientPage} />
+                        <Route exact path="/recipe-ingredients" component={RecipeIngredientPage} />
                     </Switch>
                 </main>
             </section>
