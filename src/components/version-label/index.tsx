0453e40829e3cd6f14e25b75fd828a8baa102252
lucasbravi2019 <80082740+lucasbravi2019@users.noreply.github.com>
2023-02-03 20:26:16 -0300

    Release/0.0.4

--- src/components/version-label/index.tsx
+++ src/components/version-label/index.tsx
@@ -2,7 +2,7 @@ import './index.scss'
 
 const VersionLabel = () => {
     return (
-        <p className="version-label">Version 0.0.3</p>
+        <p className="version-label">Version 0.0.4</p>
     )
 }
 
